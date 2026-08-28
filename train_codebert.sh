470a5c08b5cd0f704ea739f5fa0a4edbea6b8790
dinhngoctuyen4125 <tuyenotk4125@gmail>
2026-08-28 11:27:49 +0700

    lmao

--- train_codebert.sh
+++ train_codebert.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=codebert
 #SBATCH --output=logs/output_%j.log
 #SBATCH --error=logs/error_%j.log
-#SBATCH --partition=defq
+#SBATCH --partition=gpu
 #SBATCH --qos=short
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128G
